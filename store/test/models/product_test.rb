020d03caf88a4628876a854550ddcdfe6d9409a8
kaxla <kaylas.email.address@gmail.com>
2014-01-15 20:52:59 -0800

    added rake task

--- store/test/models/product_test.rb
+++ store/test/models/product_test.rb
@@ -3,6 +3,6 @@ require "minitest_helper"
 class ProductTest < MiniTest::Unit::TestCase
   def test_to_param
     product = Product.create!(name: "Hello World")
-    assert_equal "#{product.id}-hello-world", product.test_to_param
+    assert_equal "#{product.id}-hello-world", product.to_param
   end
 end
